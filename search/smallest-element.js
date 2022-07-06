f26e426e2af52398e0f5d0260f73438ac311c763
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-07-06 13:40:33 +0300

    Add find smallest element in an array

---
+++ search/smallest-element.js
@@ -0,0 +1,23 @@
+// Title: Smallest element in an array
+// Complexity: Time: O(n)
+//             Space: O(1)
+// Hint: Take a min variable and initialize it with the first element of the array. 
+//       Go through the array and if the element is smaller than the current min, 
+//       assign it to the min variable, otherwise keep searching.  
+
+/** 
+*@param {array} array Input array
+*@returns {element} Minimum element
+*/
+
+function maxElement(array) {
+  let min = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i];
+    }
+  }
+
+  return min;
+}

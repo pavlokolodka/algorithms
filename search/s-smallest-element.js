db06418a91c0272d795e45ec39137fead8486d7b
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-07-07 11:08:16 +0300

    Minor fix: corrected algorithm

--- search/s-smallest-element.js
+++ search/s-smallest-element.js
@@ -1,7 +1,7 @@
 // Title: Second smallest element in an array
 // Complexity: Time: O(n)
 //             Space: O(1)
-// Hint: Create variables first and second and initialize them with the first element of the array.
+// Hint: Create variables first and second and initialize them with the first element of the array and the maximum number respectively.
 //       Go through the array and if the current element is smaller than first, then update first and second, 
 //       else if the current element is smaller than second then update second 
 
@@ -10,7 +10,8 @@
 *@returns {element} Second minimum element
 */
 function nextMinElement(array) {
-  let first = second = array[0];
+  let first = array[0];
+  let second = Number.MAX_VALUE;
 
   for (let i = 1; i < array.length; i++) {
     if (first > array[i]) {

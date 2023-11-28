1ee7f890c65e24b0a9e5fa859ad6086328916bf7
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2023-11-28 12:11:11 +0200

    Update quicksort2.js

--- sorting/quicksort2.js
+++ sorting/quicksort2.js
@@ -1,7 +1,8 @@
 // Title: Quicksort
-// Complexity: Time: O(n^2) - one for loop O(n) + recursive function call O(n)
-//             Space: O(n^2) - we use 2 arrays O(n) + storing recursive function call frame O(n)
-// Hint: Select pivot element from the array and divide the remaining elements into two subarrays: less than pivot, pivot, and bigger than pivot
+// Complexity: Time: O(n^2) - one for loop O(n) + recursive function call O(n); Worst case with unbalanced partitioning.
+//             Space: O(n) - we use 2 arrays O(n) + storing recursive function call frame O(n); 
+// Hint: Select the pivot element from the array and divide the remaining elements into two subarrays: less than the pivot, pivot, and bigger than the pivot
+//       Repeat the same actions with subarrays.
  
 /** 
 *@param {array} array Unsorted array
